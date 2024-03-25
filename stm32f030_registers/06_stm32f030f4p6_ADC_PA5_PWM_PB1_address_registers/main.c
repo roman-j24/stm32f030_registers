531ac4ea65509e510f75a1434360847f3fcc60ba
roman-j24 <161938151+roman-j24@users.noreply.github.com>
2024-03-25 13:01:55 +0300

    Add files via upload
    fix code

--- stm32f030_registers/06_stm32f030f4p6_ADC_PA5_PWM_PB1_address_registers/main.c
+++ stm32f030_registers/06_stm32f030f4p6_ADC_PA5_PWM_PB1_address_registers/main.c
@@ -30,42 +30,40 @@ uint32_t val_adc = 0;
 
 int main(void)
 {
-	RCC_AHBENR  |=  (1 << 17) | (1 << 18); // GPIOAEN, GPIOBEN
-	//GPIOA_MODER &= ~(0b11 << 10);        // reset PA5 mode
-	GPIOB_MODER |=  (0b11 << 10);          // Set PA5 to analog mode
+	RCC_AHBENR  |=  (1 << 17) | (1 << 18);   // GPIOAEN, GPIOBEN
+	// Configure PA5
+	GPIOB_MODER |=  (0b11 << 10);            // Set PA5 to analog mode
+	// Configure PB1
+	GPIOB_AFRL  &= ~(0xF << 4);		 // GPIOB_AFRL, Clear bits for PB1
+	GPIOB_AFRL  |=  (1 << 4);		 // GPIOB_AFRL, Set alternate function 1 for PB1 (TIM3_CH4)
+	GPIOB_MODER |=  (2 << 2);		 // GPIOB_MODER, Set PB1 as alternate function mode
 
 	// Configure TIM3, enable clock
-	RCC_APB1ENR |=  (1 << 1);              // RCC_APB1ENR
-	// Configure TIM3, frequency, duty pulse
-	TIM3_ARR     =  1000 - 1;			   // TIM3_ARR, set auto-reload value
-	TIM3_PSC     =  80 - 1;				   // TIM3_PSC, set prescaler value
-	TIM3_CCR4    =  10;			   // TIM3_CCR4, duty
-	// Configure TIM3, PWM
-	TIM3_CCMR2  |=  (0b110 << 12);         // TIM3_CCMR1, Set PWM mode 1 for channel 4
+	RCC_APB1ENR |=  (1 << 1);                // RCC_APB1ENR
+	// Frequency, duty pulse
+	TIM3_ARR     =  1000 - 1;		 // TIM3_ARR, set auto-reload value
+	TIM3_PSC     =  80 - 1;			 // TIM3_PSC, set prescaler value
+	TIM3_CCR4    =  10;			 // TIM3_CCR4, duty
+	TIM3_CCMR2  |=  (0b110 << 12);           // TIM3_CCMR1, Set PWM mode 1 for channel 4
+	// Output and counter enable
+	TIM3_CCER   |=  (1 << 12);		 // TIM3_CCER, CC4E: Capture/Compare 4 output enable
+	TIM3_CR1    |=  (1 << 0);		 // TIM3_CR1, CEN: Counter enable
 
-	// Configure PA7 as AF
-	GPIOB_AFRL  &= ~(0xF << 4);		       // GPIOB_AFRL, Clear bits for PB1
-	GPIOB_AFRL  |=  (1 << 4);			   // GPIOB_AFRL, Set alternate function 1 for PB1 (TIM3_CH4)
-	GPIOB_MODER |=  (2 << 2);			   // GPIOB_MODER, Set PB1 as alternate function mode
-
-	// Configure TIM3, output and counter enable
-	TIM3_CCER   |=  (1 << 12);			   // TIM3_CCER, CC4E: Capture/Compare 4 output enable
-	TIM3_CR1    |=  (1 << 0);			   // TIM3_CR1, CEN: Counter enable
-
-	RCC_APB2ENR |=  (1 << 9);              // ADCEN
-	ADC_CR      |=  (1 << 4) | (1 << 0);   // ADSTP: ADC stop conversion command; ADEN
-    	ADC_SMPR    &= ~(0b111 << 0);          // reset
-    	ADC_SMPR    |=  (0b111 << 0);          // SMP[2:0]: Sampling time selection
-    	ADC_CHSELR  |=  (1 << 5);              // Select channel 5 for conversion
-    	ADC_CR      |=  (1 << 31);             // Start calibration
-    	while ((ADC_CR & (1 << 31)) != 0);     // Wait until ADCAL=0
-    	ADC_CR      |=  (1 << 0);              // ADEN: ADC enable command
+	// Configure ADC
+	RCC_APB2ENR |=  (1 << 9);                // ADCEN
+	ADC_CR      |=  (1 << 4) | (1 << 0);     // ADSTP: ADC stop conversion command; ADEN
+    	ADC_SMPR    &= ~(0b111 << 0);            // reset
+    	ADC_SMPR    |=  (0b111 << 0);            // SMP[2:0]: Sampling time selection
+    	ADC_CHSELR  |=  (1 << 5);                // Select channel 5 for conversion
+    	ADC_CR      |=  (1 << 31);               // Start calibration
+    	while ((ADC_CR & (1 << 31)) != 0);       // Wait until ADCAL=0
+    	ADC_CR      |=  (1 << 0);                // ADEN: ADC enable command
 
 	while (1)
 	{
-		ADC_CR  |=  (1 << 2);			   // Start the ADC conversion
-		while ((ADC_ISR & 1 << 2) == 0);   // Wait end of conversion
-		val_adc = ADC_DR;				   // ADC conversion result
+		ADC_CR  |=  (1 << 2);		 // Start the ADC conversion
+		while ((ADC_ISR & 1 << 2) == 0); // Wait end of conversion
+		val_adc = ADC_DR;		 // ADC conversion result
 
 		if (val_adc >= 4000 )	TIM3_CCR4 = 900;
 		if (val_adc >= 3800 &  val_adc < 4000)	TIM3_CCR4 = 400;
