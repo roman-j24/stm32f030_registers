d4bb6ff7238a71b8633298dbfa0e85ce9455094f
roman-j24 <161938151+roman-j24@users.noreply.github.com>
2024-05-16 12:42:35 +0300

    Add files via upload

--- stm32f030_registers/08_stm32f030f4p6_I2C_write_address_registers/main.c
+++ stm32f030_registers/08_stm32f030f4p6_I2C_write_address_registers/main.c
@@ -1,4 +1,4 @@
-// Created wed 15 may 2024
+// Created thu 16 may 2024
 // arm-none-eabi-as -o crt.o crt.s && arm-none-eabi-gcc -mthumb -mcpu=cortex-m0 -O0 -c -o main.o main.c && arm-none-eabi-gcc -mcpu=cortex-m0 -T linker.ld --specs=nosys.specs -Wl,--gc-sections -static --specs=nano.specs -mfloat-abi=soft -mthumb -Wl,--start-group -lc -lm -Wl,--end-group -o main.elf crt.o main.o && arm-none-eabi-objcopy -O binary main.elf main.bin && st-flash write main.bin 0x8000000 && rm *.o *.bin *.elf
 
 #include <stdint.h>
@@ -21,13 +21,14 @@
 #define GPIOA_BSRR    (*((volatile uint32_t *)0x48000018))
 #define GPIOA_AFRH    (*((volatile uint32_t *)0x48000024))
 
-void initI2C(uint8_t i2c_addr_dev)
+void initI2C(void)
 {
 	RCC_APB2ENR   |=  (1 << 0);  // SYSCFGEN
 	RCC_APB1ENR   |=  (1 << 28); // PWREN
 	RCC_AHBENR    |=  (1 << 17); // GPIOAEN
 	RCC_APB1ENR   |=  (1 << 21); // i2c clock enable
 
+	// Configure pins I2C
 	GPIOA_MODER   |=  (2 << 18) | (2 << 20);    // Set PA9, PA10 as alternate function mode
 	GPIOA_OTYPER  |=  (1 << 9)  | (1 << 10);
 	GPIOA_AFRH    &= ~(0xF << 4) & ~(0xF << 8); // Clear bits for PA9, PA10
@@ -42,21 +43,21 @@ void initI2C(uint8_t i2c_addr_dev)
 
 	I2C1_CR2 |= (0 << 25);   // AUTOEND: Automatic end mode (master mode)
 	I2C1_CR2 |= (2 << 16);   // NBYTES: Number of bytes
-	I2C1_CR2 |= (i2c_addr_dev << 1); // Slave address (master mode)
+	I2C1_CR2 |= (0x27 << 1); // Slave address (master mode)
 
 	I2C1_CR1 |= (1 << 0);    // PE: Peripheral enable
 }
 
-void writeI2C(uint8_t reg_addr, uint16_t data)
+void writeI2C(uint32_t reg_addr, uint32_t data)
 {
 	while ((I2C1_ISR & (0 << 15)) == 1); // BUSY
 	while ((I2C1_ISR & (0 << 25)) == 1); // AUTOEND
-        I2C1_CR2 |= (1 << 13);               // START: Start generation
-        while ((I2C1_ISR & (0 << 1)) == 1);  // wait for tx buffer to be empty
+	I2C1_CR2 |= (1 << 13);               // send start condition
+	while ((I2C1_ISR & (0 << 1)) == 1);  // wait for tx buffer to be empty
 	I2C1_TXDR = reg_addr;                // memory adress to start storing
 	while ((I2C1_ISR & (0 << 7)) == 1);  // TCR: Transfer complete reload
 
-	while ((I2C1_ISR & (0 << 1)) == 1);  // wait for tx buffer to be empty/ready
+	while ((I2C1_ISR & (0 << 1)) == 1);  // wait for tx buffer to be empty
 	I2C1_TXDR = data;                    // memory adress to start storing
 	while ((I2C1_ISR & (0 << 5)) == 1);  // STOPF: Stop detection flag
 	while ((I2C1_ISR & (0 << 6)) == 1);  // TC: Transfer complete (master mode) - all bytes transfered
@@ -64,18 +65,51 @@ void writeI2C(uint8_t reg_addr, uint16_t data)
 
 int main()
 {
-    uint8_t I2C_address_dev = 0x27;
-    uint8_t I2C_address_reg = 0x00;
+    initI2C();
 
-    initI2C(I2C_address_dev);
+	uint8_t delay = 3;
 
     while (1)
     {
-    	for (uint8_t i=0; i<8; i++)
-        {
-    	    writeI2C(I2C_address_reg, (1 << i)); // shift 1 from P0 to P7
-            for (uint32_t i=0; i<16000*1; i++); // dummy delay
-        }
+    	for (uint8_t i=0; i<5; i++)
+	{
+		writeI2C(0x00, 0x0f);
+		for (uint32_t i=0; i<16000*delay; i++); // dummy delay
+		writeI2C(0x00, 0xf0);
+		for (uint32_t i=0; i<16000*delay; i++); // dummy delay
+	}
+
+	for (uint8_t i=0; i<8; i++)
+	{
+		writeI2C(0x00, (5 << i));
+		for (uint32_t i=0; i<16000*delay; i++); // dummy delay
+	}
+
+	for (uint8_t i=7; i>0; i--)
+	{
+		writeI2C(0x00, (5 << i));
+		for (uint32_t i=0; i<16000*delay; i++); // dummy delay
+	}
+
+	for (uint8_t i=0; i<5; i++)
+	{
+		writeI2C(0x00, 0b01010101);
+		for (uint32_t i=0; i<16000*delay; i++); // dummy delay
+		writeI2C(0x00, 0b10101010);
+		for (uint32_t i=0; i<16000*delay; i++); // dummy delay
+	}
+
+	for (uint8_t i=0; i<8; i++)
+	{
+		writeI2C(0x00, (1 << i));
+		for (uint32_t i=0; i<16000*delay; i++); // dummy delay
+	}
+
+	for (uint8_t i=7; i>0; i--)
+	{
+		writeI2C(0x00, (1 << i));
+		for (uint32_t i=0; i<16000*delay; i++); // dummy delay
+	}
     }
 
     return 0;
